2bc729fbd34d66f0f214a66fd5b76c0890818a61
songyaru <1107941977@qq.com>
2020-08-28 16:28:04 +0800

    添加错误重连机制

--- app/src/main/java/com/syr/hiltdemo/base/BaseRepository.kt
+++ app/src/main/java/com/syr/hiltdemo/base/BaseRepository.kt
@@ -1,5 +1,6 @@
 package com.syr.hiltdemo.base
 
+import com.syr.hiltdemo.net.interceptor.RetryWithDelay
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -11,6 +12,7 @@ import io.reactivex.schedulers.Schedulers
 abstract class BaseRepository {
     fun <R> createCall(observable: Observable<R>): Observable<R> {
         return observable.subscribeOn(Schedulers.io())
+            .retryWhen(RetryWithDelay(3, 2))
             .observeOn(AndroidSchedulers.mainThread())
     }
 }
