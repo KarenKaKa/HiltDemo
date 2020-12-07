0454a084e0aa4122fac72178e6f17c5d32294cd7
KarenKaKa <1107941977@qq.com>
2020-12-07 16:06:18 +0800

    Handler demo

--- app/src/main/java/com/syr/hiltdemo/module/main/MainActivity.kt
+++ app/src/main/java/com/syr/hiltdemo/module/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.syr.hiltdemo.module.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.syr.hiltdemo.R
 import com.syr.hiltdemo.databinding.ActivityMainBinding
@@ -9,15 +12,21 @@ import com.syr.hiltdemo.module.home.HomeActivity
 import com.syr.hiltdemo.utils.showToast
 import com.syr.module_common.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var mHandler: Handler
+    private lateinit var threadHandler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         showToast("我是自定义的toast")
 
         binding = DataBindingUtil.setContentView(
@@ -27,5 +36,48 @@ class MainActivity : BaseActivity() {
         viewModel.toHome.observe(this, {
             startActivity(Intent(this, HomeActivity::class.java))
         })
+
+        mHandler = Companion.MyHandler(this) {
+            when (it.what) {
+                0 -> {
+                    Log.e("okhttp", "0 接收UI线程发送的消息")
+                }
+                else -> {
+                    Log.e("okhttp", "else 接收子线程的发送消息")
+                }
+            }
+        }
+        mHandler.sendEmptyMessage(0)
+        Log.e("okhttp", "0 Thread.name=${Thread.currentThread().name}")
+        thread {
+            Log.e("okhttp", "1 Thread.name=${Thread.currentThread().name}")
+            mHandler.sendMessage(mHandler.obtainMessage(111))
+            Looper.prepare()
+            threadHandler = Companion.MyHandler(this) {
+                when (it.what) {
+                    0 -> {
+                        Log.e("okhttp", "子线程 0 接收子线程发送的消息")
+                    }
+                    1 -> {
+                        Log.e("okhttp", "子线程 1 接收子线程发送的消息")
+                    }
+                    else -> {
+                        Log.e("okhttp", "子线程 else 接收主线程发送的消息")
+                    }
+                }
+            }
+            threadHandler.sendEmptyMessage(0)
+            Looper.loop()
+        }
+
+        GlobalScope.launch {
+            delay(1000)
+            Log.e("okhttp", "2 Thread.name=${Thread.currentThread().name}")
+            threadHandler.sendEmptyMessage(1)
+        }
+        binding.tvHandler.setOnClickListener {
+            Log.e("okhttp", "3 Thread.name=${Thread.currentThread().name}")
+            threadHandler.sendEmptyMessage(111)
+        }
     }
 }
--- module_common/src/main/java/com/syr/module_common/base/BaseActivity.kt
+++ module_common/src/main/java/com/syr/module_common/base/BaseActivity.kt
@@ -1,10 +1,13 @@
 package com.syr.module_common.base
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.alibaba.android.arouter.launcher.ARouter
+import java.lang.ref.WeakReference
 
 /**
  * @author songyaru
@@ -23,5 +26,25 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ARouter.getInstance().inject(this)
+    }
+
+    companion object {
+         class MyHandler private constructor() : Handler() {
+            private lateinit var activity: WeakReference<BaseActivity>
+            private lateinit var handleMessage: (Message) -> Unit
+
+            constructor(activity: BaseActivity, handleMessage: (Message) -> Unit) : this() {
+                this.activity = WeakReference(activity)
+                this.handleMessage = handleMessage
+            }
+
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                activity.get()?.let {
+                    if (it.isFinishing) return
+                    this.handleMessage.invoke(msg)
+                }
+            }
+        }
     }
 }
