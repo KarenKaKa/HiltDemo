8f17afe159d375fad4de3057bb02f637a9aa68a0
KarenKaKa <1107941977@qq.com>
2023-08-18 11:52:14 +0800

    stickyHeader效果

---
+++ module_custom/src/main/java/com/syr/module_custom/addressbook/PinnedItemDecoration.kt
@@ -0,0 +1,106 @@
+package com.syr.module_custom.addressbook
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * stickyHeader效果
+ * Created by songyaru on 2023/8/18.
+ */
+class PinnedItemDecoration(
+    adapter: RecyclerView.Adapter<*>,
+    val isHeader: (position: Int) -> Boolean
+) : RecyclerView.ItemDecoration() {
+    private var currentHeader: Pair<Int, RecyclerView.ViewHolder>? = null
+
+    init {
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+                currentHeader = null
+            }
+        })
+    }
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+        if (parent.adapter?.itemCount ?: 0 < 1) return
+        val topChild: View = parent.getChildAt(0)
+        val topChildPosition = parent.getChildAdapterPosition(topChild)
+        if (topChildPosition == RecyclerView.NO_POSITION) return
+        val headerView = getHeaderViewForItem(topChildPosition, parent) ?: return
+        val contactPoint = headerView.bottom + parent.paddingTop
+        val nextHeaderView = getChildInContact(parent, contactPoint) ?: return
+        if (isHeader(parent.getChildAdapterPosition(nextHeaderView))) {
+            c.translate(0f, nextHeaderView.top - headerView.height + parent.paddingTop.toFloat())
+        }
+        headerView.draw(c)
+        c.save()
+        c.translate(0f, parent.paddingTop.toFloat())
+        c.restore()
+    }
+
+    // get current headerView
+    private fun getHeaderViewForItem(itemPosition: Int, parent: RecyclerView): View? {
+        if (parent.adapter == null) return null
+        val headerPosition = getHeaderPositionForItem(itemPosition)
+        if (headerPosition == RecyclerView.NO_POSITION) return null
+        val headerType = parent.adapter?.getItemViewType(headerPosition) ?: return null
+        if (currentHeader?.first == headerPosition && currentHeader?.second?.itemViewType == headerType) {
+            return currentHeader?.second?.itemView
+        }
+        val headerHolder = parent.adapter?.createViewHolder(parent, headerType)
+        if (headerHolder != null) {
+            parent.adapter?.onBindViewHolder(headerHolder, headerPosition)
+            fixLayoutSize(parent, headerHolder.itemView)
+            currentHeader = headerPosition to headerHolder
+        }
+        return headerHolder?.itemView
+    }
+
+    // get next headerView
+    private fun getChildInContact(parent: RecyclerView, contactPoint: Int): View? {
+        var childInContact: View? = null
+        for (i in 0 until parent.childCount) {
+            val child = parent.getChildAt(i)
+            val mBounds = Rect()
+            parent.getDecoratedBoundsWithMargins(child, mBounds)
+            if (mBounds.bottom > contactPoint) {
+                if (mBounds.top <= contactPoint) {
+                    childInContact = child
+                    break
+                }
+            }
+        }
+        return childInContact
+    }
+
+    private fun fixLayoutSize(parent: ViewGroup, view: View) {
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY)
+        val heightSpec =
+            View.MeasureSpec.makeMeasureSpec(parent.height, View.MeasureSpec.UNSPECIFIED)
+        val childWidthSpec = ViewGroup.getChildMeasureSpec(
+            widthSpec, parent.paddingLeft + parent.paddingRight, view.layoutParams.width
+        )
+        val childHeightSpec = ViewGroup.getChildMeasureSpec(
+            heightSpec, parent.paddingTop + parent.paddingBottom, view.layoutParams.height
+        )
+        view.measure(childWidthSpec, childHeightSpec)
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+    }
+
+    private fun getHeaderPositionForItem(itemPosition: Int): Int {
+        var headerPosition = RecyclerView.NO_POSITION
+        var currentPosition = itemPosition
+        do {
+            if (isHeader(currentPosition)) {
+                headerPosition = currentPosition
+                break
+            }
+            currentPosition -= 1
+        } while (currentPosition >= 0)
+        return headerPosition
+    }
+}
